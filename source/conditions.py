4b5fe2288d39298719e569348d0b99478a7045f8
estereo-lab <107266133+estereo-lab@users.noreply.github.com>
2022-07-06 15:50:57 -0400

    Create conditions.py

---
+++ source/conditions.py
@@ -0,0 +1,17 @@
+# Conditions
+
+# General spectral details
+
+h = 6 # Band halfwidth in cm-1
+r1 = 950 # Lower range in cm-1 
+r2 = 1550 # Higher Range in cm-1
+blc = 0 # Baseline correction of observed VCD
+H = 627.5 # conversion factor from hartrees to kcal/mol
+T = 298 # temperature in K
+kb = 1.985875e-3 # Boltzmann constant
+
+# Special options
+
+extEnergies = False # Use energies from freeEnergies.txt on parent directory (True or False)
+
+extAbundances = False # Use conformational abundances from weights.txt on parent directory (True or False)
